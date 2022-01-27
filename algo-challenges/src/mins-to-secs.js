743cb89076ffd17fe86348971002448ab5725a63
Marley Doughty <marleydoughty@yahoo.com>
2022-01-27 22:49:00 +0000

    edabit coding challenge, return sum of two and convert mins to secs

---
+++ algo-challenges/src/mins-to-secs.js
@@ -0,0 +1,7 @@
+/* convert minutes to seconds */
+function minsToSecs(number) {
+  const convert = number * 60;
+  return convert;
+}
+const result = minsToSecs(1);
+console.log('result:', result);
---
+++ algo-challenges/src/sum-of-two.js
@@ -0,0 +1,6 @@
+/* return the sum of two numbers */
+function sum(num1, num2) {
+  return num1 + num2;
+}
+const answer = sum(10, 12);
+console.log('answer:', answer);
